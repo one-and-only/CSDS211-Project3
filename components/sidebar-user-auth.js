3c7348aa6963409cec7a3c8de62df3d8db5dbf5e
Antonios Papadakis <bdpaboi123@gmail.com>
2026-03-21 15:42:30 -0400

    Add Signin And Signup API Calls To UI

--- components/sidebar-user-auth.js
+++ components/sidebar-user-auth.js
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button"
 import { ButtonGroup } from "@/components/ui/button-group";
 import {
@@ -13,29 +13,15 @@ import {
 } from "@/components/ui/dialog"
 import { Controller, useForm } from "react-hook-form"
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
-import {
     Field,
-    FieldDescription,
-    FieldError,
     FieldGroup,
     FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
-import {
-    InputGroup,
-    InputGroupAddon,
-    InputGroupText,
-    InputGroupTextarea,
-} from "@/components/ui/input-group"
 
 function UserAuthModal({ usedForSignup = false }) {
+    const [accessToken, setAccessToken] = useState(null);
+
     const form = useForm({
         defaultValues: {
             email: "",
@@ -47,136 +33,190 @@ function UserAuthModal({ usedForSignup = false }) {
     });
 
     const onSubmit = async data => {
-        console.log(data);
+        if (
+            (usedForSignup && data.email === "") ||
+            (usedForSignup && data.firstName === "") ||
+            (usedForSignup && data.lastName === "") ||
+            data.username === "" ||
+            data.password === ""
+        ) {
+            alert("Please fill out all of the form fields");
+            return;
+        }
+
+        if (usedForSignup) {
+            const accessTokenResponse = await (await fetch(`/api/v1/users/signup`, {
+                method: "POST",
+                body: JSON.stringify({
+                    email: data.email,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    username: data.username,
+                    password: data.password,
+                })
+            })).json();
+
+            if (!accessTokenResponse.success) {
+                alert(`Failed to log in:\n\n${accessTokenResponse.error}`);
+                return;
+            }
+
+            document.cookie = `accessToken=${accessTokenResponse.accessToken}; path=/; max-age=2592000; SameSite=Lax`; // 30 days
+
+            return;
+        }
+
+        try {
+            const accessTokenResponse = await (await fetch(`/api/v1/users/login?username=${encodeURIComponent(data.username)}&password=${encodeURIComponent(data.password)}`)).json();
+            if (!accessTokenResponse.success) {
+                alert(`Failed to log in:\n\n${accessTokenResponse.error}`);
+                return;
+            }
+
+            document.cookie = `accessToken=${accessTokenResponse.accessToken}; path=/; max-age=2592000; SameSite=Lax`; // 30 days
+        } catch (e) {
+            alert("Network request failed. Please check your network connection, refresh the page, and try again.");
+            return;
+        }
     };
 
+    useEffect(() => {
+        if (!document.cookie.includes("accessToken")) return;
+
+        setAccessToken(decodeURIComponent(document.cookie).split(";").map(x => x.trim()).filter(x => x.indexOf("accessToken") === 0)[0].substring(12));
+    }, []);
+
     return (
-        <Dialog>
-            <DialogTrigger asChild>
-                {usedForSignup ? <Button variant="outline">Sign Up</Button> : <Button color="primary">Sign In</Button>}
-            </DialogTrigger>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>{usedForSignup ? "Create your account" : "Welcome back!"}</DialogTitle>
-                    <DialogDescription style={{ whiteSpace: 'pre-wrap' }}>
-                        {
-                            usedForSignup ? "Fill out this form to create your account and begin chatting with your favorite people!"
-                                :
-                                "Glad you came back today. We missed you!\nAs usual, fill out the form below to log in:"
-                        }
-                    </DialogDescription>
-                    <form id="signin-signup" onSubmit={form.handleSubmit(onSubmit)}>
-                        {usedForSignup && <FieldGroup>
-                            <Controller
-                                name="email"
-                                control={form.control}
-                                render={({ field, fieldState }) => (
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="signup-signin-email-field">
-                                            Email
-                                        </FieldLabel>
-                                        <Input
-                                            {...field}
-                                            id="signup-signin-email-field"
-                                            placeholder="user@example.com"
-                                            autoComplete="email"
-                                        />
-                                    </Field>
-                                )}
-                            />
-                        </FieldGroup>}
-                        <FieldGroup>
-                            <Controller
-                                name="username"
-                                control={form.control}
-                                render={({ field, fieldState }) => (
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="signup-signin-username-field">
-                                            Username
-                                        </FieldLabel>
-                                        <Input
-                                            {...field}
-                                            id="signup-signin-username-field"
-                                            placeholder="RandomUser007"
-                                            autoComplete="username"
-                                        />
-                                    </Field>
-                                )}
-                            />
-                        </FieldGroup>
-                        <FieldGroup>
-                            <Controller
-                                name="firstName"
-                                control={form.control}
-                                render={({ field, fieldState }) => (
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="signup-signin-firstname-field">
-                                            First Name
-                                        </FieldLabel>
-                                        <Input
-                                            {...field}
-                                            id="signup-signin-firstname-field"
-                                            placeholder="Distinguished"
-                                            autoComplete="given-name"
-                                        />
-                                    </Field>
-                                )}
-                            />
-                        </FieldGroup>
-                        <FieldGroup>
-                            <Controller
-                                name="lastName"
-                                control={form.control}
-                                render={({ field, fieldState }) => (
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="signup-signin-lastname-field">
-                                            Last Name
-                                        </FieldLabel>
-                                        <Input
-                                            {...field}
-                                            id="signup-signin-lastname-field"
-                                            placeholder="Person"
-                                            autoComplete="family-name"
-                                        />
-                                    </Field>
-                                )}
-                            />
-                        </FieldGroup>
-                        <FieldGroup>
-                            <Controller
-                                name="password"
-                                control={form.control}
-                                render={({ field, fieldState }) => (
-                                    <Field data-invalid={fieldState.invalid}>
-                                        <FieldLabel htmlFor="signup-signin-password-field">
-                                            Password
-                                        </FieldLabel>
-                                        <Input
-                                            {...field}
-                                            id="signup-signin-password-field"
-                                            placeholder="totallySecure123!"
-                                            autoComplete="off"
-                                            type="password"
-                                        />
-                                    </Field>
-                                )}
-                            />
-                        </FieldGroup>
-                        <Button style={{ marginTop: "20px" }} type="submit" form="signin-signup">
-                            {usedForSignup ? "Sign Up" : "Sign In"}
-                        </Button>
-                    </form>
-                </DialogHeader>
-            </DialogContent>
-        </Dialog>
+        <>
+            {accessToken === null ? <Dialog>
+                <DialogTrigger asChild>
+                    {usedForSignup ? <Button variant="outline">Sign Up</Button> : <Button color="primary">Sign In</Button>}
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>{usedForSignup ? "Create your account" : "Welcome back!"}</DialogTitle>
+                        <DialogDescription style={{ whiteSpace: 'pre-wrap' }}>
+                            {
+                                usedForSignup ? "Fill out this form to create your account and begin chatting with your favorite people!"
+                                    :
+                                    "Glad you came back today. We missed you!\nAs usual, fill out the form below to log in:"
+                            }
+                        </DialogDescription>
+                        <form id="signin-signup" onSubmit={form.handleSubmit(onSubmit)}>
+                            {usedForSignup && <FieldGroup>
+                                <Controller
+                                    name="email"
+                                    control={form.control}
+                                    render={({ field, fieldState }) => (
+                                        <Field data-invalid={fieldState.invalid}>
+                                            <FieldLabel htmlFor="signup-signin-email-field">
+                                                Email
+                                            </FieldLabel>
+                                            <Input
+                                                {...field}
+                                                id="signup-signin-email-field"
+                                                placeholder="user@example.com"
+                                                autoComplete="email"
+                                            />
+                                        </Field>
+                                    )}
+                                />
+                            </FieldGroup>}
+                            <FieldGroup>
+                                <Controller
+                                    name="username"
+                                    control={form.control}
+                                    render={({ field, fieldState }) => (
+                                        <Field data-invalid={fieldState.invalid}>
+                                            <FieldLabel htmlFor="signup-signin-username-field">
+                                                Username
+                                            </FieldLabel>
+                                            <Input
+                                                {...field}
+                                                id="signup-signin-username-field"
+                                                placeholder="RandomUser007"
+                                                autoComplete="username"
+                                            />
+                                        </Field>
+                                    )}
+                                />
+                            </FieldGroup>
+                            {usedForSignup && <FieldGroup>
+                                <Controller
+                                    name="firstName"
+                                    control={form.control}
+                                    render={({ field, fieldState }) => (
+                                        <Field data-invalid={fieldState.invalid}>
+                                            <FieldLabel htmlFor="signup-signin-firstname-field">
+                                                First Name
+                                            </FieldLabel>
+                                            <Input
+                                                {...field}
+                                                id="signup-signin-firstname-field"
+                                                placeholder="Distinguished"
+                                                autoComplete="given-name"
+                                            />
+                                        </Field>
+                                    )}
+                                />
+                            </FieldGroup>
+                            }
+                            {usedForSignup && <FieldGroup>
+                                <Controller
+                                    name="lastName"
+                                    control={form.control}
+                                    render={({ field, fieldState }) => (
+                                        <Field data-invalid={fieldState.invalid}>
+                                            <FieldLabel htmlFor="signup-signin-lastname-field">
+                                                Last Name
+                                            </FieldLabel>
+                                            <Input
+                                                {...field}
+                                                id="signup-signin-lastname-field"
+                                                placeholder="Person"
+                                                autoComplete="family-name"
+                                            />
+                                        </Field>
+                                    )}
+                                />
+                            </FieldGroup>
+                            }
+                            <FieldGroup>
+                                <Controller
+                                    name="password"
+                                    control={form.control}
+                                    render={({ field, fieldState }) => (
+                                        <Field data-invalid={fieldState.invalid}>
+                                            <FieldLabel htmlFor="signup-signin-password-field">
+                                                Password
+                                            </FieldLabel>
+                                            <Input
+                                                {...field}
+                                                id="signup-signin-password-field"
+                                                placeholder="totallySecure123!"
+                                                autoComplete="off"
+                                                type="password"
+                                            />
+                                        </Field>
+                                    )}
+                                />
+                            </FieldGroup>
+                            <Button style={{ marginTop: "20px" }} type="submit" form="signin-signup">
+                                {usedForSignup ? "Sign Up" : "Sign In"}
+                            </Button>
+                        </form>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+                : (<h2>{usedForSignup ? "" : "You are now logged in! Functionality will be added soon..."}</h2>)
+            }
+        </>
     );
 }
 
 export default function SidebarUserAuth() {
     return (
         <>
-            <h2>Chats will appear once you are logged in. We hope you enjoy your stay.</h2>
-            <br />
             <div style={{ flex: 1, margin: "auto" }}>
                 <ButtonGroup>
                     <ButtonGroup>
