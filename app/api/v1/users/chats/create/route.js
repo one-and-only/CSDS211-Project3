7275989d3602b0360866697257a13e96033f72e9
Antonios Papadakis <bdpaboi123@gmail.com>
2026-03-31 21:54:45 -0400

    Add `Create Chat` Route Handler

---
+++ app/api/v1/users/chats/create/route.js
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import { prisma } from "../../../../../../lib/prisma";
+
+export async function POST(request, context) {
+    const searchParams = await request.nextUrl.searchParams;
+    const accessToken = searchParams.get("accessToken");
+    const targetUsername = searchParams.get("targetUsername");
+
+    if (accessToken === null) {
+        return NextResponse.json({
+            success: false,
+            error: "Failed to provide a valid access token"
+        }, { status: 401 });
+    }
+
+    const user = await prisma.users.findFirst({
+        where: {
+            accessToken
+        },
+        select: {
+            userId: true
+        }
+    });
+
+    if (user === null) {
+        return NextResponse.json({
+            success: false,
+            error: "Failed to provide a valid access token"
+        }, { status: 401 });
+    }
+
+    const targetUser = await prisma.users.findFirst({
+        where: {
+            username: targetUsername
+        },
+        select: {
+            userId: true
+        }
+    });
+
+    await prisma.chats.create({
+        data: {
+            initiatorUserId: user.userId,
+            targetUserId: targetUser.userId
+        }
+    });
+
+    return NextResponse.json({
+        success: true
+    });
+}
